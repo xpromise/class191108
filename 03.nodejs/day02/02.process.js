7658ff303ff046b7bdb55dc03c9a4157021563af
xiongjian <webjsforyou@163.com>
2020-03-04 10:24:33 +0800

    process和path模块的使用

---
+++ 03.nodejs/day02/02.process.js
@@ -0,0 +1,46 @@
+/*
+  process 进程
+    argv 获取运行指令的文件后面的命令行参数
+    argv0 获取nodejs程序目录
+    execArgv 获取运行指令的文件前面的命令行参数
+    env 获取系统环境变量
+      环境变量：Path --> 遍历每个路径，找到程序运行
+    cwd() 当前文件的文件夹绝对路径
+    exit([code]) 退出进程
+*/
+
+// 运行指令: node --inspect .\02.process.js hello process
+/*
+  [
+    // nodejs程序目录
+    'D:\\software\\nodejs\\node.exe',  
+    // 当前运行文件绝对路径
+    'C:\\Users\\XiongJian\\Desktop\\class191108\\03.nodejs\\day02\\02.process.js' 
+    'hello',
+    'process'
+  ]
+*/
+// console.log(process.argv);
+// console.log(process.argv0);
+// ['--inspect']
+// console.log(process.execArgv);
+// console.log(process.env);
+
+// console.log(process.cwd()); // C:\Users\XiongJian\Desktop\class191108\03.nodejs\day02
+
+let i = 0;
+
+/*
+  怎么终止nodejs程序：
+    1. 手动终止： ctrl + c
+    2. process.exit() 
+*/  
+
+setInterval(() => {
+  console.log('定时器执行了:', ++i);
+  if (i > 10) {
+    process.exit();
+  }
+}, 1000)
+
+
---
+++ 03.nodejs/day02/03.path.js
@@ -0,0 +1,21 @@
+/*
+  path 路径
+    专门用来出来路径问题
+    path.relative(from, to) 用来处理相对路径
+    path.resolve([...paths]) 用来处理绝对路径
+  
+  Buffer和process是global全局变量，所以可以直接使用
+  而nodejs其他模块，不需要下载，只需要引入才能使用  
+*/
+const path = require('path');
+
+// console.log(path.relative('./public', 'index.html')); // ..\index.html
+
+console.log(path.resolve('index.html')); // C:\Users\XiongJian\Desktop\class191108\03.nodejs\day02\index.html
+
+// 需求：在path.js文件，获取process.js文件的绝对路径
+// const dirname = __dirname;
+// console.log(path.resolve(__dirname, '02.process.js')); // C:\Users\XiongJian\Desktop\class191108\03.nodejs\day02\02.process.js
+
+// 需求：在path.js文件，获取git.md文件的绝对路径
+console.log(path.resolve(__dirname, '../../', '02.作业&笔记/', '笔记/git.md'));
