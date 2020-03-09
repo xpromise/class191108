9602f082435c747a4aafbe46fa5c85523812e504
xiongjian <webjsforyou@163.com>
2020-03-09 18:56:02 +0800

    feat: eslint\lint-staged\husky功能

--- 03.nodejs/day05/05.eslint/.eslintrc.js
+++ 03.nodejs/day05/05.eslint/.eslintrc.js
@@ -18,8 +18,9 @@ module.exports = {
 	rules: {
 		// 详细配置规则
 		// semi是一个eslint规则，代表代码后面以 ; 结尾
-		semi: "error" // 没有的 以 ; 结尾 就报红色的错误
+		semi: "error", // 没有的 以 ; 结尾 就报红色的错误
 		// semi: 'warnning', // 没有的 以 ; 结尾 就报黄色的警告
 		// semi: 'false', // 不检查
+		"space-infix-ops": "error" // 要求操作符周围有空格
 	}
 };
