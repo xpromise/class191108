81082a050164b9fc67b234c937d0ac81fef50b62
xiongjian <webjsforyou@163.com>
2020-03-09 11:25:48 +0800

    feat: 增加命令行参数功能

---
+++ 03.nodejs/day05/04.静态资源服务器-yargs/src/cli/index.js
@@ -0,0 +1,30 @@
+/*
+  配置命令行参数
+*/
+// npm i yargs
+const yargs = require("yargs");
+
+const argv = yargs
+  .usage("server [options]")
+  .option("p", {
+    // 配置指令选项
+    alias: "port", // 配置别名
+    describe: "端口号", // 描述
+    default: 3000 // 默认值
+  })
+  .option("h", {
+    // 配置指令选项
+    alias: "host", // 配置别名
+    describe: "主机名", // 描述
+    default: "localhost" // 默认值
+  })
+  .option("d", {
+    alias: "root",
+    describe: "运行项目的更目录",
+    default: process.cwd()
+  })
+  .version() // 增加 -v 指令， 查看版本号
+  .alias("v", "version") // 对 v 进行配置别名：version
+  .help().argv; // 增加 --help 指令，查看帮助
+
+module.exports = argv;
