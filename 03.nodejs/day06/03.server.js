2fc3228f7faf3a23a729c6405fdbf91828fdf5d4
xiongjian <webjsforyou@163.com>
2020-03-10 17:01:23 +0800

    feat: add ajax

--- 03.nodejs/day06/03.server.js
+++ 03.nodejs/day06/03.server.js
@@ -4,13 +4,44 @@ const server = http.createServer((req, res) => {
   // 打印GET请求参数
   console.log(req.url);
 
+  const contentType = req.headers["content-type"];
+
+  // 接受post请求体参数
+  let data = "";
+
+  req
+    .on("data", chunk => {
+      data += chunk.toString();
+    })
+    .on("end", () => {
+      console.log("接受到了post请求数据", data);
+      // {"name":"jack","age":18}
+      // 需要将数据转化成js对象
+      // console.log(JSON.parse(data));
+
+      if (contentType === "application/json") {
+        console.log(JSON.parse(data));
+      }
+
+      if (contentType === "application/x-www-form-urlencoded") {
+        // name=jack&age=18
+        const result = data.split("&").reduce((p, c) => {
+          const [key, value] = c.split("=");
+          p[key] = value;
+          return p;
+        }, {});
+        // { name: 'jack', age: '18' }
+        console.log(result);
+      }
+    });
   // 会产生跨域错误，解决它
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   // 返回响应
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain;charset=utf8");
-  res.end("服务器返回的响应~");
+  res.end("服务器返回的响应5555~");
 });
 
 server.listen(3000, "localhost", err => {
