ddcff8436ddf54a49214778f8b37bb79b3bf459f
xiongjian <webjsforyou@163.com>
2020-03-11 15:23:41 +0800

    feat: add then

--- 03.nodejs/day07/05.自定义Promise/01.MyPromise.js
+++ 03.nodejs/day07/05.自定义Promise/01.MyPromise.js
@@ -16,6 +16,8 @@
     // 初始化实例对象的结果值
     _self._result = undefined;
 
+    _self._callbacks = {};
+
     // 要求Promise对象的状态只能修改一次 并且 只能pending --> resolved/rejected
     function resolve(value) {
       if (_self._status === "pending") {
@@ -23,6 +25,13 @@
         _self._status = "resolved";
         // 给Promise对象结果值
         _self._result = value;
+        // 调用成功的回调
+        // 同步调用
+        // _self._callbacks.onResolved();
+        // 异步调用
+        setTimeout(function() {
+          _self._callbacks.onResolved(value);
+        }, 0);
       }
     }
 
@@ -32,6 +41,10 @@
         _self._status = "rejected";
         // 给Promise对象结果值
         _self._result = reason;
+        // 异步调用
+        setTimeout(function() {
+          _self._callbacks.onRejected(reason);
+        }, 0);
       }
     }
 
@@ -39,7 +52,32 @@
     exector(resolve, reject);
   }
 
-  MyPromise.prototype.then = function() {};
+  MyPromise.prototype.then = function(onResolved, onRejected) {
+    const _self = this;
+    
+    // 返回一个新Promise对象
+    return new MyPromise(function (resolve, reject) {
+      // 新Promise对象的状态看什么  onResolved 或 onResolved 的执行结果
+      _self._callbacks.onResolved = function (value) {
+        // 得到内部函数的结果值
+        const result = onResolved(value);
+        // 判断result是否是Promise对象
+        if (result instanceof MyPromise) {
+          // 是promise
+          // 怎么知道result内部的状态？
+          result.then(function(value) {
+            resolve(value);
+          }, function (reason) {
+            reject(reason);
+          })
+        } else {
+          // 不是promise
+        }
+      };
+      _self._callbacks.onRejected = onRejected;
+    })
+  };
+
   MyPromise.prototype.catch = function() {};
 
   MyPromise.resolve = function() {};
