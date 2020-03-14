9fbb96c400d2768fa588f934387dc9d196403605
xiongjian <webjsforyou@163.com>
2020-03-14 08:56:53 +0800

    feat: add Promise.race/allSettled

--- 03.nodejs/day08/01.MyPromise.js
+++ 03.nodejs/day08/01.MyPromise.js
@@ -82,6 +82,12 @@
   MyPromise.prototype.catch = function(onRejected) {
     return this.then(null, onRejected);
   };
+  /*  
+    不管成功/失败都要触发
+  */
+  MyPromise.prototype.finally = function(callback) {
+    return this.then(callback, callback);
+  };
 
   // 返回值是一个promise，状态默认是成功的，也有可能是初始化/失败
   MyPromise.resolve = function(value) {
@@ -140,8 +146,72 @@
     });
   };
 
-  MyPromise.race = function () {}
-  MyPromise.allSettled = function () {}
+  /*
+    返回值promise状态 看传入promise对象中的第一个发生状态变化的状态
+  */
+  MyPromise.race = function(promises) {
+    return new MyPromise((resolve, reject) => {
+      promises.forEach(promise => {
+        // promise可能是number（不是一个promise对象）
+        // 将其包装成promise对象
+        MyPromise.resolve(promise).then(resolve, reject);
+      });
+    });
+  };
+
+  /*
+    等所有传入promise对象状态都发生变化
+    最终返回值一定是成功的状态promise，并里面包含所有传入promise的结果值（一一对应）
+  */
+  MyPromise.allSettled = function(promises) {
+    return new MyPromise((resolve) => {
+      const promisesLength = promises.length;
+      // 收集所有成功promise的结果值
+      const result = new Array(promisesLength);
+      // 成功promise的数量
+      let resolvedCount = 0;
+
+      // 遍历数组
+      promises.forEach((promise, index) => {
+        // 判断是否是promise
+        // 判断promise对象状态
+        /* MyPromise.resolve(promise).then(
+          value => {
+            // 收集结果值
+            result[index] = value;
+            resolvedCount++;
+            if (resolvedCount === promisesLength) {
+              // 说明所有都成功了
+              resolve(result);
+            }
+          },
+          reason => {
+            // 收集结果值
+            result[index] = reason;
+            resolvedCount++;
+            if (resolvedCount === promisesLength) {
+              // 说明所有都成功了
+              resolve(result);
+            }
+          }
+        ); */
+        /*
+          then 成功
+          catch 失败
+          finally 成功/失败
+        */
+        MyPromise.resolve(promise).finally(value => {
+          // 收集结果值
+          result[index] = value;
+          resolvedCount++;
+          if (resolvedCount === promisesLength) {
+            // 说明所有都成功了
+            resolve(result);
+          }
+        });
+      });
+    });
+  };
 
   w.MyPromise = MyPromise;
 })(window);
