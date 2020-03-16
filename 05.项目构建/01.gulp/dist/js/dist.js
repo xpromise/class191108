adb3122c1ced2f54ffb79fc743916eaa6afc0909
xiongjian <webjsforyou@163.com>
2020-03-16 11:46:44 +0800

    feat: gulp-less

---
+++ 05.项目构建/01.gulp/dist/js/dist.js
@@ -0,0 +1,29 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+"use strict";
+
+var _sum = _interopRequireDefault(require("./sum"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+console.log((0, _sum["default"])(1, 2, 3, 4));
+},{"./sum":2}],2:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+function sum() {
+  for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+    args[_key] = arguments[_key];
+  }
+
+  return args.reduce(function (p, c) {
+    return p + c;
+  }, 0);
+}
+
+var _default = sum;
+exports["default"] = _default;
+},{}]},{},[1])
--- 05.项目构建/01.gulp/gulpfile.js
+++ 05.项目构建/01.gulp/gulpfile.js
@@ -3,7 +3,9 @@
     1. 搭建开发环境：让代码在浏览器运行即可
       - js代码(ES6模块化)
         gulp-babel 将ES6模块化编译成commonjs模块化
-
+        gulp-browserify 将commonjs模块化编译成浏览器能识别的模块化
+      - less代码
+        gulp-less 将less编译成css
     2. 搭建生产环境
 */
 /*
@@ -12,6 +14,9 @@
 */
 const gulp = require("gulp");
 const babel = require("gulp-babel");
+const browserify = require('gulp-browserify');
+const rename = require("gulp-rename");
+const less = require('gulp-less');
 
 // 注册任务
 // gulp.task(任务名称, 任务回调函数)
@@ -21,11 +26,34 @@ gulp.task("babel", () => {
     .src("src/js/*.js") // 将src/js/index.js文件读取
     .pipe(
       // 对可读流中的文件数据进行babel处理
-      babel({ 
+      babel({
         presets: ["@babel/preset-env"]
       })
     )
     .pipe(gulp.dest("dist/js")); // 将可读流中文件输出到"dist/js"目录下
 });
 
+gulp.task('browserify', function() {
+  // 必须加return 否则报错
+  return gulp.src('dist/js/index.js')
+      .pipe(browserify()) // 将Commonjs模块化编译成浏览器能识别的模块化
+      .pipe(rename('dist.js')) // 对流中的文件进行重命名
+      .pipe(gulp.dest('dist/js'))
+});
+
+gulp.task('less', function () {
+  return gulp.src('src/less/*.less')
+    .pipe(less()) // 将less编译成css
+    .pipe(gulp.dest('dist/css'));
+});
+
 // 运行任务：gulp 任务名称
+
+// 配置统一任务
+// 当你执行js任务时，实际上执行的是  gulp.series 任务，
+// 而 gulp.series 任务实际上执行的是 'babel', 'browserify'
+// 特点是：同步执行  一般会慢一点
+gulp.task('js', gulp.series(['babel', 'browserify']));
+// 特点是：异步/并行执行 一般会快一点
+// gulp.task('js', gulp.parallel(['babel', 'browserify']));
+gulp.task('dev', gulp.parallel(['js', 'less']));
