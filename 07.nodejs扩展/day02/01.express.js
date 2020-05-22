4021e1248d2f45c68d0f739f494a09a7c8d1cf13
xiongjian <webjsforyou@163.com>
2020-05-22 08:58:54 +0800

    express的基本使用

---
+++ 07.nodejs扩展/day02/01.express.js
@@ -0,0 +1,43 @@
+// 引入express
+const express = require("express");
+// 创建APP应用对象：APP包含express框架核心功能
+const app = express();
+
+// 处理请求的功能
+app.get("/", (request, response) => {
+  /*
+    request 请求对象
+    response 响应对象
+  */
+  console.log(111);
+  // 返回响应
+  response.end("success111~~~");
+});
+/*
+  get 代表请求方式
+  /login 代理请求路径
+*/
+app.get("/login", (request, response) => {
+  console.log(222);
+  // 返回响应
+  response.end("success222~~~");
+});
+/*
+  post 代表请求方式
+  /register 代理请求路径
+*/
+app.post("/register", (request, response) => {
+  console.log(333);
+  // 返回响应
+  response.end("success333~~~");
+});
+
+// 监听端口号，启动服务
+app.listen(9527, "localhost", (err) => {
+  if (err) {
+    console.log("服务器启动失败:", err);
+    return;
+  }
+
+  console.log("服务器启动成功，请访问：http://localhost:9527");
+});
